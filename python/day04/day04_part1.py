4b01725e33da979f6cd5d9eee7eaa5ebf3a847fa
Kris Austin <kris.austin@draft2digital.com>
2023-12-04 10:38:52 -0600

    changed day 4 to use sets, for conciseness

--- python/day04/day04_part1.py
+++ python/day04/day04_part1.py
@@ -4,14 +4,10 @@ total = 0
 with open('day04_input.txt') as f:
     for line in f:
         _, winning, have = re.split(r"[:|]", line)
-        winning = winning.split()
-        have = have.split()
+        winning = set(winning.split())
+        have = set(have.split())
 
-        win_count = 0
-        for win in winning:
-            if win in have:
-                win_count += 1
-
+        win_count = len(winning.intersection(have))
         if win_count:
             total += 2**(win_count-1)
 
--- python/day04/day04_part2.py
+++ python/day04/day04_part2.py
@@ -7,16 +7,12 @@ with open('day04_input.txt') as f:
     for line in f:
         _, card, winning, have = re.split(r"Card |[:|]", line)
         card = int(card)
-        winning = winning.split()
-        have = have.split()
+        winning = set(winning.split())
+        have = set(have.split())
 
         cards[card] += 1
 
-        win_count = 0
-        for win in winning:
-            if win in have:
-                win_count += 1
-
+        win_count = len(winning.intersection(have))
         if win_count:
             for i in range(card+1, card+win_count+1):
                 cards[i] += cards[card]
