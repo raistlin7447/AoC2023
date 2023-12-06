d7029d336bb471255960615c651495ca6f62c43c
Kris Austin <kris.austin@draft2digital.com>
2023-12-06 08:31:35 -0600

    added Day 6

---
+++ python/day06/day06_part1.py
@@ -0,0 +1,20 @@
+import math
+
+with open('day06_input.txt') as f:
+    lines = f.read().splitlines()
+    times = [int(i) for i in lines[0].split() if i.isnumeric()]
+    dists = [int(i) for i in lines[1].split() if i.isnumeric()]
+
+    total_wins = []
+    for i, time in enumerate(times):
+        wins = 0
+        for attempt in range(1, time+1):
+            speed = attempt
+            remaining_time = time-attempt
+            distance = remaining_time * speed
+            if distance > dists[i]:
+                wins += 1
+
+        total_wins.append(wins)
+
+    print(math.prod(total_wins))
